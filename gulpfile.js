4d42001eb583ac66055bb79c318aed59d58522a8
Abayomi igwubor <igwubor@outlook.com>
2019-08-06 11:46:24 +0100

    reducedCssInDistFolder

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var uglifycss = require('gulp-uglifycss');
 gulp.task('css', function () {
     gulp.src('./css/*.css')
       .pipe(uglifycss({
-        "maxLineLen": 80,
         "uglyComments": true
       }))
       .pipe(gulp.dest('./dist/'));
